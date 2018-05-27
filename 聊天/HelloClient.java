56112bf402f2d268cffd33c72821c24075faa688
RoyXinyun <38729250+RoyXinyun@users.noreply.github.com>
2018-05-27 17:22:37 +0800

    Add files via upload

--- 聊天/HelloClient.java
+++ 聊天/HelloClient.java
@@ -1,4 +1,8 @@
-package ttcp;
+package Chat;
+/**
+ * 客户端应用，使用线程，可以启发多个线程对象，从而实现多个客户端连接
+ *  输入ip和端口连接，可发送消息，发送消息和接受消息在不同的线程中，可以同时发生
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,7 +14,6 @@ import java.net.Socket;
 class ClientThread implements Runnable{
     BufferedReader buff = null;
     Socket client = null;
-    InetAddress remAdd;
 
     public void run(){
         try {
@@ -23,17 +26,15 @@ class ClientThread implements Runnable{
             int port = Integer.parseInt(str);
             //建立连接
             client = new Socket(ip,port);
-
             //启动发送消息线程
             new Thread(new SendThread(client)).start();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
     public static void main(String[] args) {
+        //启动客户端线程
         new Thread(new ClientThread()).start();
     }
 }
@@ -48,16 +49,14 @@ class SendThread implements Runnable {
     }
     public void run(){
         try{
+            //初始化输入，输出流，从控制台读取消息，发送到服务器
             BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
             PrintStream out = new PrintStream(client.getOutputStream());
             while(true){
                 System.out.println("我： ");
                 String str = buff.readLine();
-                if(str == "")
-                    Thread.sleep(10000);
                 out.println(str);
             }
-
         }
         catch (Exception e){
             e.printStackTrace();
--- 聊天/HelloServer.java
+++ 聊天/HelloServer.java
@@ -1,26 +1,27 @@
-package ttcp;
+package Chat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 
+/**
+ * 服务器端，可以等待客户端连接，接受所有链接的客户端消息
+ */
 class HelloServer {
     public static void main(String[] args) throws Exception{
         //建立服务器
         ServerSocket server = null;
         Socket user = null;
         server = new ServerSocket(8888);
-
+        //等待客户端连接
         while(true){
             System.out.println("服务器运行，等待客户端连接 ");
             user = server.accept();
-
+            //启动接受消息线程
             new Thread(new ReceiveThread(user)).start();
         }
-
     }
 }
-
 /**
  * 接收消息线程
  */
@@ -29,7 +30,7 @@ class ReceiveThread implements Runnable {
     public ReceiveThread(Socket client){
         this.client = client;
     }
-
+    //初始化，用来接受消息输送到控制台
     public void run(){
         try{
             while(true){
